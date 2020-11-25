0aaac97e42e4aba1ebc81882093a225b4bae861a
Stefan Dendorfer <dendorfer_stefan@gmx.de>
2020-11-25 05:58:36 +0100

    finished first version

--- snapchatscraper.py
+++ snapchatscraper.py
@@ -59,15 +59,21 @@ def DownloadMemories(path,dest):
                 print("failed to create directory")
 
     src = UnzipFolder(path,dest)
-    DownloadMemories(src,dest)
+    DownloadAll(src,dest)
     
 
 def UnzipFolder(path, dest):
     src = os.path.join(dest,"src")
     print(src)
-    os.mkdir(src)
-    with zipfile36.ZipFile(path, 'r') as zip_ref:
-        zip_ref.extractall(src)
+    if not os.path.isfile(src):
+        try:
+            os.mkdir(src)
+            with zipfile36.ZipFile(path, 'r') as zip_ref:
+                zip_ref.extractall(src)
+        except:
+            print("failed to create src directory")
+            return src
+
     return src
 
 
@@ -138,7 +144,99 @@ def GetFolderStructure(html):
 
 
 def DownloadAll(src, dest):
-    return True
+    src = os.path.join(src,"html","memories_history.html")
+    file = open(src, "r")
+    html = file.read()
+    pq = PyQuery(html)
+    rows = pq("tr")
+    triples = []
+    for row in rows.items():
+        pq2 = PyQuery(row.html())
+        tds = pq2("td")
+        if not len(tds.text())==0:
+            i = 0
+            for td in tds.items():
+                i += 1
+                if (i==1):
+                    yearmonth = td.text()
+                    month =yearmonth[5:7]
+                    year = yearmonth[0:4]
+                if (i==2):
+                    type = td.text()
+                if (i==3):
+                    ref = td("a").attr["href"]
+                    if ref is not None:
+                        ref = ref.replace("javascript:downloadMemories('","")
+                        ref = ref.replace("');","")
+                    else:
+                        ref = None
+            triple = (year,month,yearmonth[0:16].replace(":","-").replace(" ","_"),type,ref)
+            triples.append(triple)
+    DownloadTriples(triples,src,dest)
+
+
+
+def DownloadTriples(triples,src,dest):
+    yearmonths = []
+    for item in triples:
+        yearmonth = item[0] + "-" + item[1]
+        yearmonths.append(yearmonth)
+    CreateFolders(yearmonths,dest)
+    imageCounter = -1
+    count = len(triples)
+    for item in triples:
+        imageCounter +=1
+        print("downloading " + item[3] + " " + str(imageCounter) + " of total of " + str(count) + " files...")
+        destPath = os.path.join(dest,item[0],item[1])
+        filename = item[2] + "_" + str(imageCounter)
+        if item[3]=="VIDEO":
+            fileextension = ".mp4"
+        if item[3]=="PHOTO":
+            fileextension = ".jpg"
+        finalPath = os.path.join(destPath,filename+fileextension)
+        url = item[4]
+        snapchatResponse = requests.post(url)
+        r = requests.get(snapchatResponse.content)
+        open(finalPath, 'wb').write(r.content)
+
+
+
+
+            
+def CreateFolders(yearmonths,dest):
+    for item in yearmonths:
+        year = item[0:4]
+        month = item[5:7]
+        yearPath = os.path.join(dest,year)
+        monthPath = os.path.join(yearPath,month)
+        if not os.path.isdir(yearPath):
+            os.mkdir(yearPath)
+        if os.path.isdir(yearPath):
+            if not os.path.isdir(monthPath):
+                os.mkdir(monthPath)
+    
+
+
+
+
+    # refs = []
+    # for f in files.items():
+    #     ref = f.attr["href"]
+    #     if ref is not None:
+    #         ref = ref.replace("javascript:downloadMemories('","")
+    #         ref = ref.replace("');","")
+    #         refs.append(ref)
+    # print(refs[1])
+    # url = refs[1]
+    # snapchatResponse = requests.post(url)
+    # text = snapchatResponse.content.decode("utf-8") 
+    # print(snapchatResponse.content)
+    # r = requests.get(snapchatResponse.content)
+    # if text.__contains__("mp4"):
+    #     open(os.path.join(dest,"test2.mp4"), 'wb').write(r.content)
+    # if text.__contains__("jpg"):
+    #     open(os.path.join(dest,"test2.jpg"), 'wb').write(r.content)
+
 
 
 
